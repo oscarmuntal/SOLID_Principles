3b336012b54c3d9e11c27d00a2bce8add324eb73
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-28 20:01:46 +0200

    Liskov Substitution Principle: The Before code which violates this principle.

--- SOLID/3_LiskovSubstitutionPrinciple/LSP_Before.swift
+++ SOLID/3_LiskovSubstitutionPrinciple/LSP_Before.swift
@@ -7,3 +7,46 @@
 
 import Foundation
 
+fileprivate class Rectangle {
+    var width: Float = 0
+    var height: Float = 0
+    
+    func set(width: Float) {
+        self.width = width
+    }
+    
+    func set(height: Float) {
+        self.height = height
+    }
+    
+    func calculateArea() -> Float {
+        return width * height
+    }
+}
+
+fileprivate class Square: Rectangle {
+    override func set(width: Float) {
+        self.width = width
+        self.height = width
+    }
+    
+    override func set(height: Float) {
+        self.width = width
+        self.height = width
+    }
+}
+
+/// This breaks the Liskov Substitution Principle
+fileprivate func setSizeAndPrintArea(of rectangle: Rectangle) {
+    rectangle.set(height: 5)
+    rectangle.set(width: 4)
+    print(rectangle.calculateArea())
+}
+
+fileprivate func example() {
+    let rectangle = Rectangle()
+    setSizeAndPrintArea(of: rectangle)  // prints 20
+    
+    let square = Square()
+    setSizeAndPrintArea(of: square)     // prints 16
+}
