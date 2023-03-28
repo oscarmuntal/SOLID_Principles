b964a1faf9e6bb45f04487f4e15f26e04a908674
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-28 19:37:49 +0200

    Open-Close Principle: The Before code which violates this principle.

--- SOLID/SOLID/2_OpenClosedPrinciple/OCP_Before.swift
+++ SOLID/SOLID/2_OpenClosedPrinciple/OCP_Before.swift
@@ -7,3 +7,17 @@
 
 import Foundation
 
+/// This code works but it is not extendable nor maintainable because it breaks the second SOLID principle which is the Open Closed Principle.
+fileprivate class PaymentManager {
+    func makeCashPayment(amount: Double) {
+        // perform
+    }
+    
+    func makeVisaPayment(amount: Double) {
+        // perform
+    }
+    
+    func makeMasterCardPayment(amount: Double) {
+        // perform
+    }
+}
