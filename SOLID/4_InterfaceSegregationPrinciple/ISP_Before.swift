cdd5eaf13fd322321e5a795c32117dd73e15057b
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-28 20:21:56 +0200

    Interface Segregation Principle: The Before code which violates this principle.

--- SOLID/4_InterfaceSegregationPrinciple/ISP_Before.swift
+++ SOLID/4_InterfaceSegregationPrinciple/ISP_Before.swift
@@ -7,3 +7,41 @@
 
 import Foundation
 
+fileprivate protocol Worker {
+    func work()
+    func eat()
+}
+
+fileprivate class Human: Worker {
+    func work() {
+        print("Working...")
+    }
+    
+    func eat() {
+        print("Eating...")
+    }
+}
+
+fileprivate class Robot: Worker {
+    func work() {
+        print("Working...")
+    }
+    
+    /// Here we have a function that if it's going to be executed it shouldn't be able to be executed
+    func eat() {
+        fatalError("Robot does not eat!")
+    }
+}
+
+fileprivate func example() {
+    let human = Human()
+    human.work()         // prints "Working..."
+    human.eat()          // prints "Eating..."
+
+    /// Interface Segregation: No code should be forced to depend on methods it doesn't use
+    let robot = Robot()
+    robot.work()
+    robot.eat()     /// This robot is forced to use the eat function but it does not use it
+}
+
+
