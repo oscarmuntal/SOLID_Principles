7649bf6cb06606ba41171380aa5962f15ece4acf
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-28 19:33:14 +0200

    Single Responsibility Principle: The "After" code, which accomplishes correctly this principle

--- SOLID/SOLID/1_SingleResponsibilityPrinciple/SRP_After.swift
+++ SOLID/SOLID/1_SingleResponsibilityPrinciple/SRP_After.swift
@@ -17,3 +17,40 @@ A module or a class should have only one responsibility, and that responsibility
 
 import Foundation
 
+/// Here we have 3 classes with only one responsibility
+fileprivate class APIHandler {
+    func loadData() -> Data {
+        return Data()
+    }
+}
+
+fileprivate class ParseHandler {
+    func parse(data: Data) -> Any {
+        return ""
+    }
+}
+
+fileprivate class StorageHandler {
+    func save(model: Any) {
+        // save here
+    }
+}
+
+/// Here we have this class which handles all the previous three and has it's own single responsibility which is handling the three previous steps
+fileprivate class DataHandler {
+    let apiHandler: APIHandler
+    let parseHandler: ParseHandler
+    let storageHandler: StorageHandler
+    
+    init(apiHandler: APIHandler, parseHandler: ParseHandler, storageHandler: StorageHandler) {
+        self.apiHandler = apiHandler
+        self.parseHandler = parseHandler
+        self.storageHandler = storageHandler
+    }
+    
+    func handle() {
+        let data = apiHandler.loadData()
+        let model = parseHandler.parse(data: data)
+        storageHandler.save(model: model)
+    }
+}
