cedaeb98d993470a84740aef07d40c6d51651200
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-28 19:17:20 +0200

    Adding the five folders for each SOLID principle with the before and after empty files

---
+++ SOLID/SOLID/1_SingleResponsibilityPrinciple/SRP_After.swift
@@ -0,0 +1,19 @@
+//
+//  SRP_After.swift
+//  SOLID_Principles
+//
+//  Created by Alex Nagy on 18.12.2021.
+//
+
+/*:
+ ## Single Responsibility Principle
+ [Wikipedia](https://en.wikipedia.org/wiki/Single_responsibility_principle)
+
+ "There should never be more than one reason for a class to change."
+ 
+A module or a class should have only one responsibility, and that responsibility should be entirely encapsulated by that module/class.
+
+ */
+
+import Foundation
+
