3c1821e8262c64cc4e1f3cce7312b5629945b6fa
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-28 20:32:37 +0200

    Dependency Inversion Principle: The "Before" code which violates this principle.

--- SOLID/5_DepencencyInversionPrinciple/DIP_Before.swift
+++ SOLID/5_DepencencyInversionPrinciple/DIP_Before.swift
@@ -7,3 +7,25 @@
 
 import Foundation
 
+fileprivate struct Employee {
+    func work() {
+        print("Working...")
+    }
+}
+
+fileprivate struct Employer {
+    var employees: [Employee]
+    
+    func manage() {
+        employees.forEach { employee in
+            employee.work()
+        }
+    }
+}
+
+fileprivate func example() {
+    let employer = Employer(employees: [Employee()])
+    employer.manage()
+}
+
+
